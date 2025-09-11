3dcd8f143f23bfce6020511aa271b013325dbf57
Dhiva Ryan H <176643369+d-hardine@users.noreply.github.com>
2025-09-11 17:48:13 +0700

    initial react commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      count is {count}
+      <br />
+      <button onClick={() => setCount(count + 1)}>click me</button>
+    </div>
+  )
+}
+
+export default App
